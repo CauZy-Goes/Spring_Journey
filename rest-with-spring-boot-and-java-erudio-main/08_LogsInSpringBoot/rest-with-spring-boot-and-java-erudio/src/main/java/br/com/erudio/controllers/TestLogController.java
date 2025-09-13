18581c1185d71743725a4153b949fa7a94a9a8c3
CauZy-Goes <cauafariasdev@gmail.com>
2025-09-13 18:19:44 -0300

    add logs

--- rest-with-spring-boot-and-java-erudio-main/08_LogsInSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/TestLogController.java
+++ rest-with-spring-boot-and-java-erudio-main/08_LogsInSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/TestLogController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestLogController {
 
+
+    //
     private Logger logger = LoggerFactory.getLogger(TestLogController.class.getName());
 
     @GetMapping("/test")
