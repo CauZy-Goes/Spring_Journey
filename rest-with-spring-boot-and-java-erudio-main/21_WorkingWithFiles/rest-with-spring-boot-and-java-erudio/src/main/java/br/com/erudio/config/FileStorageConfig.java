8e748d0a5c694c52bf506eb75fd259ab78b96daf
CauZy-Goes <cauafariasdev@gmail.com>
2025-09-19 17:42:29 -0300

    add arquivos notes

--- rest-with-spring-boot-and-java-erudio-main/21_WorkingWithFiles/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/FileStorageConfig.java
+++ rest-with-spring-boot-and-java-erudio-main/21_WorkingWithFiles/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/FileStorageConfig.java
@@ -3,7 +3,7 @@ package br.com.erudio.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration // configura~ção do arquivos
 @ConfigurationProperties(prefix = "file")
 public class FileStorageConfig {
 
